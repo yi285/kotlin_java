5731f05a7061e53b3b82b140671767f6bc077506
yi285 <angellopez2006.24@gmail.com>
2025-10-28 12:24:39 +0100

    actu

--- src/aprender_java.java
+++ src/aprender_java.java
@@ -4,6 +4,6 @@ public class aprender_java {
 
     public static void main(String[] args) {
         System.out.println("Cuando sepa usar los dos hare cosas aqui");
-        System.out.println("Bienvenidos a mi mundo chicos buenos");
+        System.out.println("Bienvenidos a mi mundo chicos buenos y majos");
     }
 }
