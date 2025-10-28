9c3d41c921b0e843b7b2cf0e63dce3b2c5a86c11
anlo546 <anlo546@vidalibarraquer.net>
2025-10-28 11:58:47 +0100

    actualizacion

--- src/aprender_java.java
+++ src/aprender_java.java
@@ -4,5 +4,6 @@ public class aprender_java {
 
     public static void main(String[] args) {
         System.out.println("Cuando sepa usar los dos hare cosas aqui");
+        System.out.println("Bienvenidos a mi mundo");
     }
 }
